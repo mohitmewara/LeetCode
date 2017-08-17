7111a616c07bdd499a158acfcf3a0e3a48a10abc
Mohit <mohitmewara1@gmail.com>
2017-08-17 10:27:09 -0700

    Create ProductOfArray.java

---
+++ 238. Product of Array Except Self/ProductOfArray.java
@@ -0,0 +1,16 @@
+public class ProductOfArray {
+    public int[] productExceptSelf(int[] nums) {
+        int[] prod = new int[nums.length];
+        int temp=1;
+        for(int i=0;i<nums.length;i++){
+            prod[i] = temp;
+            temp *= nums[i];
+        }
+        temp=1;
+        for(int i=nums.length-1;i>=0;i--){
+            prod[i] *= temp;
+            temp *= nums[i];
+        }
+        return prod;
+    }
+}
