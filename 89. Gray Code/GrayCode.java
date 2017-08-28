0af07ca6a38c598d573dc38bdab67f76915ab9ac
Mohit <mohitmewara1@gmail.com>
2017-08-28 09:33:34 -0400

    Create GrayCode.java

---
+++ 89. Gray Code/GrayCode.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GrayCode {
+    public List<Integer> grayCode(int n) {
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i=0;i<1<<n;i++)
+            list.add(i ^ (i/2));
+        return list;
+    }    
+
+}
