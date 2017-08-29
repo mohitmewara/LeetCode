b277e0d0a03fea23aa1aac4d50701d2a1bbf2196
Mohit <mohitmewara1@gmail.com>
2017-08-29 11:23:48 -0400

    Create SearchMatrix.java

---
+++ 240 Search a 2D Matrix II/SearchMatrix.java
@@ -0,0 +1,10 @@
+class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length==0) return false;
+        int i=0;
+        int j = matrix[0].length-1;
+        while(i < matrix.length && j >=0){
+            if(matric)
+        }
+    }
+}
